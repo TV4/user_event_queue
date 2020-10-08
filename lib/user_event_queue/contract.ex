aed32a2702a45f8cba5aa24810774cf7c0bf64d2
robinch <robinch88@gmail.com>
2020-10-08 15:46:07 +0200

    Add added and remove item id in Contract

--- lib/user_event_queue/contract.ex
+++ lib/user_event_queue/contract.ex
@@ -1,5 +1,5 @@
 defmodule UserEventQueue.Contract do
-  defstruct [:customer_id, :order_id]
+  defstruct [:customer_id, :order_id, :added_item_id, :removed_item_id]
 
   @type t :: %__MODULE__{}
 end
