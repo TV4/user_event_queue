fef3d46e8848ae4ce5870572a556bcdafbc54158
robinch <robinch88@gmail.com>
2020-10-09 15:46:49 +0200

    Add currency to Contract

--- lib/user_event_queue/contract.ex
+++ lib/user_event_queue/contract.ex
@@ -1,5 +1,5 @@
 defmodule UserEventQueue.Contract do
-  defstruct [:customer_id, :order_id, :added_item_id, :removed_item_id]
+  defstruct [:customer_id, :order_id, :added_item_id, :removed_item_id, :currency]
 
   @type t :: %__MODULE__{}
 end
